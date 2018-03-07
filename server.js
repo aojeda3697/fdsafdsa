287b36dd68df1b46f3c5f8cd4018bce4303e313c
Abdul Ojeda <aojeda@Abduls-MacBook-Pro.local>
2018-03-06 22:16:38 -0800

    abcd

--- server.js
+++ server.js
@@ -11,12 +11,12 @@ console.log("loaded");
 // ------------------------------------------------------------------------------------------------------
 // Clients
 
-app.use(function(req, res, next) {
-  res.header("Access-Control-Allow-Origin", "*");
-  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
-  res.header('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE');
-  next();
-});
+// app.use(function(req, res, next) {
+//   res.header("Access-Control-Allow-Origin", "*");
+//   res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+//   res.header('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE');
+//   next();
+// });
 
 app.get("/clients", function(request, response) {
   model.Client.find().then(function (clients){
